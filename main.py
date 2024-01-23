17edfd57d116f392cbe61cfba7c5eedc402abfb8
IgaStach <156843216+IgaStach@users.noreply.github.com>
2024-01-23 19:11:03 +0100

    Class Snake added

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ def open_game(username):
     snake_color = 'blue'
     food_color = 'red'
     background_color = 'light green'
+
+    class Snake:
+        def __init__(self):
+            self.body_size = body_parts
+            self.coordinates = []
+            self.squares = []
+            self.direction = "down"
+
+            for _ in range(0, body_parts):
+                self.coordinates.append([0, 0])
+            for x, y in self.coordinates:
+                square = canvas.create_rectangle(x, y, x + space_size, y + space_size, fill=snake_color, tag="snake")
+                self.squares.append(square)
